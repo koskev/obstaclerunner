0f0668caa2ed8999a1f14f7768491e83e838070f
Kevin <kevin@kokev.de>
2024-03-25 22:19:12 +0100

    Move some stuff into player file

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,8 +1,15 @@
 use bevy::prelude::*;
-use bevy_rapier2d::dynamics::LockedAxes;
-use leafwing_input_manager::{input_map::InputMap, InputManagerBundle};
+use bevy_rapier2d::{
+    dynamics::{LockedAxes, Velocity},
+    geometry::Collider,
+};
+use leafwing_input_manager::{action_state::ActionState, input_map::InputMap, InputManagerBundle};
 
-use crate::{input::PlayerAction, physics::ControllerBundle};
+use crate::{
+    animation::{AnimationBundle, AnimationIndices, AnimationTimer, Model},
+    input::PlayerAction,
+    physics::{ColliderChild, ControllerBundle},
+};
 
 use super::character::CharacterBundle;
 
@@ -45,5 +52,75 @@ impl Default for PlayerBundle {
             input: InputManagerBundle::with_map(input_map),
             locked_axes: LockedAxes::ROTATION_LOCKED | LockedAxes::TRANSLATION_LOCKED_X,
         }
+    }
+}
+fn player_jump(mut q_player: Query<(&mut Velocity, &ActionState<PlayerAction>), With<Player>>) {
+    for (mut velocity, input_data) in &mut q_player {
+        if input_data.just_pressed(&PlayerAction::Jump) {
+            info!("jump");
+            velocity.linvel = Vec2::new(0.0, 1.0) * 300.0;
+        }
+    }
+}
+
+fn player_duck() {}
+
+fn player_death() {}
+
+fn setup_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let idle_set = asset_server.load("tiles/dungeon.png");
+
+    let layout = TextureAtlasLayout::from_grid(
+        Vec2::new(16.0, 32.0),
+        9,
+        1,
+        None,
+        Some(Vec2::new(8.0 * 16.0, 0.0)),
+    );
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let animation_indices = AnimationIndices { first: 0, last: 6 };
+    let bundle = Model {
+        animation: AnimationBundle {
+            indices: animation_indices.clone(),
+            timer: AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        },
+        spritesheet: SpriteSheetBundle {
+            texture: idle_set,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: animation_indices.first,
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 15.0, 0.0)),
+            ..Default::default()
+        },
+        collider: ColliderChild {
+            collider: Collider::capsule_y(3.5, 5.0),
+            transform: TransformBundle {
+                local: Transform::from_translation(Vec3::new(0.0, -8.0, 0.0)),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+    };
+
+    // spawn player
+    let player_bundle = PlayerBundle::default();
+
+    let entity_commands = commands.spawn(player_bundle);
+    bundle.spawn(entity_commands);
+}
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_player)
+            .add_systems(Update, player_jump)
+            .add_systems(Update, player_duck)
+            .add_systems(Update, player_death);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use bevy_rapier2d::{
 };
 use entities::{
     enemy::Enemy,
-    player::{Player, PlayerBundle, PlayerCameraBundle},
+    player::{Player, PlayerBundle, PlayerCameraBundle, PlayerPlugin},
 };
 use input::PlayerAction;
 use leafwing_input_manager::{
@@ -37,11 +37,10 @@ fn main() {
         .add_plugins(InputManagerPlugin::<PlayerAction>::default())
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins(PlayerPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Startup, setup_player)
         .add_systems(Startup, setup_world)
         .add_systems(Startup, setup_large_enemy)
-        .add_systems(Update, (player_duck, player_jump, on_death))
         .add_systems(Update, update_world)
         .add_systems(Update, animate_sprites)
         .add_systems(Update, collision)
@@ -255,66 +254,6 @@ fn setup_large_enemy(
 
     commands.insert_resource(Models { models: model_map });
 }
-
-fn setup_player(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let idle_set = asset_server.load("tiles/dungeon.png");
-
-    let layout = TextureAtlasLayout::from_grid(
-        Vec2::new(16.0, 32.0),
-        9,
-        1,
-        None,
-        Some(Vec2::new(8.0 * 16.0, 0.0)),
-    );
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-    let animation_indices = AnimationIndices { first: 0, last: 6 };
-    let bundle = Model {
-        animation: AnimationBundle {
-            indices: animation_indices.clone(),
-            timer: AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-        },
-        spritesheet: SpriteSheetBundle {
-            texture: idle_set,
-            atlas: TextureAtlas {
-                layout: texture_atlas_layout,
-                index: animation_indices.first,
-            },
-            transform: Transform::from_translation(Vec3::new(0.0, 15.0, 0.0)),
-            ..Default::default()
-        },
-        collider: ColliderChild {
-            collider: Collider::capsule_y(3.5, 5.0),
-            transform: TransformBundle {
-                local: Transform::from_translation(Vec3::new(0.0, -8.0, 0.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-    };
-
-    // spawn player
-    let player_bundle = PlayerBundle::default();
-
-    let entity_commands = commands.spawn(player_bundle);
-    bundle.spawn(entity_commands);
-}
-
-fn player_jump(mut q_player: Query<(&mut Velocity, &ActionState<PlayerAction>), With<Player>>) {
-    for (mut velocity, input_data) in &mut q_player {
-        if input_data.just_pressed(&PlayerAction::Jump) {
-            info!("jump");
-            velocity.linvel = Vec2::new(0.0, 1.0) * 300.0;
-        }
-    }
-}
-
-fn player_duck() {}
-
-fn on_death() {}
 
 fn collision(
     mut commands: Commands,
